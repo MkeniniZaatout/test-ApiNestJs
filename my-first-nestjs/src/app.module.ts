0116209d8715968096ba785c9262601227ae1712
MkeniniZaatout <mkeninismael2@gmail.com>
2022-03-07 00:39:40 +0100

    fixe pb database connexion

--- my-first-nestjs/src/app.module.ts
+++ my-first-nestjs/src/app.module.ts
@@ -4,23 +4,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthorModule } from './author/author.module';
 import { ArticleModule } from './article/article.module';
+import { ArticleService } from './article/article.service';
+import { Article } from './article/article.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mariadb',
+      type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
-      database: 'tuto_nest',
+      password: '',
+      database: 'test_idruide',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
+    TypeOrmModule.forFeature([Article]),
     AuthorModule,
     ArticleModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,ArticleService],
 })
 export class AppModule {}
--- my-first-nestjs/src/author/author.module.ts
+++ my-first-nestjs/src/author/author.module.ts
@@ -4,11 +4,13 @@ import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
 import { ArticleController } from 'src/article/article.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleService } from 'src/article/article.service';
+import { Article } from 'src/article/article.entity';
 // import { AuthorRepository } from 'src/';
 
 @Module({
-  imports:[TypeOrmModule.forFeature([Author])],
-  providers: [AuthorService],
+  imports:[TypeOrmModule.forFeature([Author]), TypeOrmModule.forFeature([Article])],
+  providers: [AuthorService, ArticleService],
   controllers: [AuthorController, ArticleController]
 })
 export class AuthorModule {}
